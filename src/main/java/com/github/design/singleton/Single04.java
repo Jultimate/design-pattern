d2270e9475d6ae8d9dae6a57e26949e58e9a380d
chongzhao66 <chongzhao66@gmail.com>
2021-12-06 11:54:11 +0800

    singlton-pattern done

---
+++ src/main/java/com/github/design/singleton/Single04.java
@@ -0,0 +1,21 @@
+package com.github.design.singleton;
+
+/**
+ * @Description:
+ * @Author: CHONG
+ * @CreateTime: 2021/12/05 22:40
+ * @Email: chongzhao66@gmail.com
+ */
+public class Single04 {
+
+    // private contructor
+    private Single04(){}
+
+    static class Single{
+        private static final Single04 INSTANCE = new Single04();
+    }
+
+    public static void main(String[] args){
+        Single04 instance = Single.INSTANCE;
+    }
+}
